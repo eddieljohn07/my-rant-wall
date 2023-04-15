e8f2e09409ea711c31366a0ccc95a99132b22d54
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2023-04-16 03:43:43 +0800

    update margin of hs

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ clearButton.addEventListener('click', () => {
 	textContainer.innerHTML = '';
 });
 
+function addBouncingText() {
+	const text = textInput.value.trim();
+	if (text !== '') {
+		const bouncingText = document.createElement('span');
+		bouncingText.textContent = text;
+		bouncingText.classList.add('bouncingText');
+		bouncingText.style.left = `${Math.random() * 100}%`;
+		bouncingText.style.top = `${Math.random() * 100}%`;
+		textContainer.appendChild(bouncingText);
+	}
+	textInput.value = '';
+}
+
