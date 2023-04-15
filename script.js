64ce6b597a29b5ba9f099988b7050ce438541bcb
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2023-04-16 03:34:36 +0800

    initialized variables

---
+++ script.js
@@ -0,0 +1,4 @@
+const textInput = document.getElementById('textInput');
+const addButton = document.getElementById('addButton');
+const clearButton = document.getElementById('clearButton');
+const textContainer = document.getElementById('textContainer');
