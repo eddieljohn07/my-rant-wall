d5c35f3129489096518c42156f6aa061f68f0c86
eddieljohn07 <100287870+eddieljohn07@users.noreply.github.com>
2023-04-16 03:36:28 +0800

    add event listener in addButton

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const textInput = document.getElementById('textInput');
 const addButton = document.getElementById('addButton');
 const clearButton = document.getElementById('clearButton');
 const textContainer = document.getElementById('textContainer');
+
+addButton.addEventListener('click', () => {
+	addBouncingText();
+});
